4c53b142b37d6624bbbfc84035256358b284e8c1
Dhillips19 <daniel.phillips@outlook.ie>
2023-04-26 17:00:55 +0100

    Created Word Class

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -1,5 +1,43 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 public class Word {
     
+    public String word;
+    public ArrayList<Follow> wordArrayList;
+    
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    public Word()
+        {
+
+        }
+    
+    public Word(ArrayList<Follow> wordArrayList, String word)
+    {
+        this.word = word;
+        this.wordArrayList = wordArrayList;
+    }
+
+    public void setWord(String word)
+    {
+        this.word = word;
+    }
+    public String getWord()
+    {
+        return word;
+    }
+    public void setWordArrayList(ArrayList<Follow> wordArrayList)
+    {
+        this.wordArrayList = wordArrayList;
+    }
+    public ArrayList<Follow> getWordArrayList()
+    {
+        return wordArrayList;
+    }
 }
